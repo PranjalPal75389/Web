40c5b69eed8c87a4538c2ea9c9c150ba71e93638
PranjalPal75389 <100915131+PranjalPal75389@users.noreply.github.com>
2024-01-27 13:12:21 +0530

    Delete Programs/Randomnubmer generator directory

--- Programs/Randomnubmer generator/script.js
+++
@@ -1,9 +0,0 @@
-// random number
-const randomlabel=document.getElementById("randlabel");
-const randombtn=document.getElementById("randombtn");
-randombtn.onclick=function(){
-    randomnumber=Math.floor(Math.random()*6)+1;
-    
-    randomlabel.textContent=randomnumber;
-    randombtn.style.backgroundColor="lightblue"
-}
