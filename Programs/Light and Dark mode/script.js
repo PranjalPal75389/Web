4e6e6b271a767fc8576cdde56bbd3069174b2998
PranjalPal75389 <palpranjal75389@gmail.com>
2024-02-13 21:04:45 +0530

    u

--- Programs/Light and Dark mode/script.js
+++ Programs/Light and Dark mode/script.js
@@ -2,7 +2,7 @@ const image=document.getElementById("image")
 let mode="white";
 function modeChange(){
     if(mode=="white"){
-        image.src='img/NightMode.png';
+        image.src='img/NightMode1.png';
     document.body.style.backgroundColor="grey";
     document.body.style.color="white";
     mode="black";
