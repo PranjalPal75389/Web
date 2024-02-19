09fb38a8f696a119d8f07b2713688ba771800138
PranjalPal75389 <palpranjal75389@gmail.com>
2024-02-19 23:28:38 +0530

    improved logic

--- Tip_calculator/script.js
+++ Tip_calculator/script.js
@@ -1,14 +1,32 @@
-const billAmount=document.getElementById("billAmount");
-const tipPercent=document.getElementById("tipPercent");
-const totalAmount=document.getElementById("totalAmount");
+const totalAmount = document.getElementById("totalAmount");
+const message = document.getElementById("message");
+const submitBtn = document.getElementById("submitBtn");
 
-function tipCalculate(){
-    
-    Amount=Number(billAmount.value);
-    Percentage=Number(tipPercent.value);
-    // tip=Amount*Percentage/100;
-    // total=Amount+tip; 
-    total=Amount*(1+Percentage/100);
-    totalAmount.textContent=`Your Total: ${total.toFixed(2)}`;
-    
-}
+submitBtn.addEventListener("click", tipCalculate);
+
+function tipCalculate() {
+    let Amount = document.getElementById("billAmount").value;
+    let Percentage = document.getElementById("tipPercent").value;
+    if (Amount == "") {
+        message.textContent = "Bill CAN'T BE EMPTY!!!";
+        return 0;
+    }
+    else if (isNaN(Amount) || isNaN(Percentage)) {
+        alert("NO LETTERS ONLY NUMBERS!!!")
+        message.textContent = "NO LETTERS ONLY NUMBERS!!!";
+        return 0;
+    }
+    else if (Amount <= 0 || Percentage < 0) {
+        message.textContent = "Bill can't be zero or negative";
+        return 0;
+    }
+    else {
+        message.remove();
+        total = Amount * (1 + Percentage / 100);
+        // tip=Amount*Percentage/100;
+        // total=Amount+tip; 
+
+        console.log(total.toFixed(2));
+        totalAmount.innerHTML = `Your Total: <b> ₹ ${total.toFixed(2)} </b> (Tip = <b>₹ ${(total - Amount).toFixed(2)} </b>)`;
+    }
+}  
